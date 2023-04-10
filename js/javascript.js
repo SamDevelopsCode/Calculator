ca7f7b885701e2542a4da3672a9c859ca8cfe886
samdevelopscode <fousek.sam@gmail.com>
2023-04-10 15:16:22 -0500

    Added logic structure for all operator buttons.

--- js/javascript.js
+++ js/javascript.js
@@ -7,8 +7,8 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
     
-
-let displayValue = "0";
+let firstNum = "";
+let secondNum = "";
 
 function operate(operator, num1, num2)
 {
@@ -30,16 +30,43 @@ function createButtonEvents(array)
 {
     for (let i = 0; i < array.length; i++) {
         
-        if(array[i].textContent != "=") {
+        if (array[i].textContent == "C") {
             array[i].addEventListener("click", () => {
-                displayStuff(array[i].textContent);
-            })        
+                console.log("Clicked CLEAR.");
+            })
+        }
+        else if (array[i].textContent == "+") {
+            array[i].addEventListener("click", () => {
+                console.log("Clicked add.");
+                firstNum = parseFloat(display.textContent); //TODO: Need to show only one number on the screen at a time
+                console.log(firstNum);
+            })
+        }
+        else if (array[i].textContent == "-") {
+            array[i].addEventListener("click", () => {
+                console.log("Clicked subtract.");
+            })
+        }
+        else if (array[i].textContent == "*") {
+            array[i].addEventListener("click", () => {
+                console.log("Clicked multiply.");
+            })
+        }
+        else if (array[i].textContent == "/") {
+            array[i].addEventListener("click", () => {
+                console.log("Clicked divide.");
+            })
         }
         else if (array[i].textContent == "=") {
             array[i].addEventListener("click", () => {
                 console.log("Clicked enter. Doing an operation now.");
             })
         }
+        else {
+            array[i].addEventListener("click", () => {
+                    displayStuff(array[i].textContent);
+                })        
+        }        
     }
 }
 
@@ -48,4 +75,16 @@ function displayStuff(thing)
     display.insertAdjacentText("beforeend", thing);
 }
 
+//                      ENTRY POINT
+//--------------------------------------------------------------------
 createButtonEvents(buttonArray);
+
+
+//Psuedocode
+//When an operator is clicked, store the number before the operator
+//When the = sign is clicked, store the textContent inside of the display into a variable
+//this will be a string, I need to then parse through the string, get all the numbers and store them into variables
+//will need to check for what operation is being done as well in between the numbers
+
+//so convert the string into numbers and operators, do the operations
+//convert back into a string and display again
