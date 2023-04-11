19f30bd58bda60913848727a2bdb8dcd01000741
samdevelopscode <fousek.sam@gmail.com>
2023-04-11 08:42:28 -0500

    Refactored button assingment to be much cleaner.

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,13 @@
 const display = document.querySelector("#display");
+const clearButton = document.querySelector(".clear");
+const addButton = document.querySelector(".add");
+const subtractButton = document.querySelector(".subtract");
+const multiplyButton = document.querySelector(".multiply");
+const divideButton = document.querySelector(".divide");
+const equalsButton = document.querySelector(".equals");
+
 const buttons = document.querySelectorAll("button");
-const buttonArray = Array.from(buttons);
+const numbersButtonArray = Array.from(buttons);
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
@@ -26,48 +33,48 @@ function operate(operator, num1, num2)
     }
 }
 
-function createButtonEvents(array)
-{
-    for (let i = 0; i < array.length; i++) {
-        
-        if (array[i].textContent == "C") {
-            array[i].addEventListener("click", () => {
-                console.log("Clicked CLEAR.");
-            })
-        }
-        else if (array[i].textContent == "+") {
-            array[i].addEventListener("click", () => {
-                console.log("Clicked add.");
-                firstNum = parseFloat(display.textContent); //TODO: Need to show only one number on the screen at a time
-                console.log(firstNum);
-            })
-        }
-        else if (array[i].textContent == "-") {
-            array[i].addEventListener("click", () => {
-                console.log("Clicked subtract.");
-            })
-        }
-        else if (array[i].textContent == "*") {
-            array[i].addEventListener("click", () => {
-                console.log("Clicked multiply.");
-            })
-        }
-        else if (array[i].textContent == "/") {
-            array[i].addEventListener("click", () => {
-                console.log("Clicked divide.");
-            })
-        }
-        else if (array[i].textContent == "=") {
-            array[i].addEventListener("click", () => {
-                console.log("Clicked enter. Doing an operation now.");
-            })
-        }
-        else {
-            array[i].addEventListener("click", () => {
-                    displayStuff(array[i].textContent);
-                })        
-        }        
+function assignButtonEvents()
+{    // Loop that assigns events to each button
+    for (let i = 0; i < numbersButtonArray.length; i++)
+    {        
+        numbersButtonArray[i].addEventListener("click", () => 
+        {
+            displayStuff(numbersButtonArray[i].textContent);
+        })
     }
+
+    clearButton.addEventListener("click", () => 
+    {
+        console.log("Clicked CLEAR.");
+    })    
+
+    addButton.addEventListener("click", () => 
+    {
+        console.log("Clicked add.");
+        firstNum = parseFloat(display.textContent); // <-----Feel like if I do this, but its like I need to do this in each operator...Hmm...
+        console.log(firstNum);
+    })
+
+    subtractButton.addEventListener("click", () => 
+    {
+        console.log("Clicked subtract.");
+    })
+
+    multiplyButton.addEventListener("click", () => 
+    {
+        console.log("Clicked multiply.");
+    })
+
+    divideButton.addEventListener("click", () => 
+    {
+        console.log("Clicked divide.");
+    })
+
+    equalsButton.addEventListener("click", () => 
+    {
+        console.log("Clicked enter. Doing an operation now.");
+    })
+    
 }
 
 function displayStuff(thing)
@@ -77,14 +84,9 @@ function displayStuff(thing)
 
 //                      ENTRY POINT
 //--------------------------------------------------------------------
-createButtonEvents(buttonArray);
+assignButtonEvents();
 
 
-//Psuedocode
-//When an operator is clicked, store the number before the operator
-//When the = sign is clicked, store the textContent inside of the display into a variable
-//this will be a string, I need to then parse through the string, get all the numbers and store them into variables
-//will need to check for what operation is being done as well in between the numbers
-
-//so convert the string into numbers and operators, do the operations
+//When an operator is clicked, store the current number
+//When the = sign is clicked, store the completed operation
 //convert back into a string and display again
