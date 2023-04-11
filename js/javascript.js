f342a16faf0a13735d116f3b2eac0f392ab5e352
samdevelopscode <fousek.sam@gmail.com>
2023-04-11 15:41:28 -0500

    Added return value to operate function.

--- js/javascript.js
+++ js/javascript.js
@@ -14,50 +14,60 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
     
-let firstNum = "";
-let secondNum = "";
+let a = null;
+let b = null;
+let total = 0;
 
 function operate(operator, num1, num2)
 {
     if (operator == "+") {
-        add(num1, num2);
+        total = add(num1, num2);
+        return total;
     }
     else if (operator == "-") {
-        subtract(num1, num2);
+        total =subtract(num1, num2);
+        return total;
     }
     else if (operator == "*") {
-        multiply(num1, num2);
+        total = multiply(num1, num2);
+        return total;
     }
     else if (operator == "/") {
-        divide(num1, num2);
+        total = divide(num1, num2);
+        return total;
     }
 }
 
 function assignButtonEvents()
-{    // Loop that assigns events to each button
+{
+    console.log("a: ", a)
+
     for (let i = 0; i < numbersButtonArray.length; i++)
     {        
         numbersButtonArray[i].addEventListener("click", () => 
-        {
-            display(numbersButtonArray[i].textContent);
+        {            
+            display(numbersButtonArray[i].textContent);                                
         })
     }
 
     clearButton.addEventListener("click", () => 
     {
         console.log("Clicked CLEAR.");
+        displayContainer.textContent = "";
         //clear display and refresh all variables holding values
     })    
 
     addButton.addEventListener("click", () => 
     {
         console.log("Clicked add.");
-        firstNum = parseFloat(displayContainer.textContent); // 1. when clicked, convert the string into a float and store into a variable
+        a = parseInt(displayContainer.textContent);
+        console.log("a: ", a);
+        operator = "+"; 
         // 2. override what is shown in the display if more numbers are clicked 
         // 3. then either another operator or the equals will be pressed
         // 4. if operator is clicked, show the value of the operation before
         // 5. if the equals is clicked, show the value of the operation
-        //const value = operate("+", firstNum, secondNum)// complete the operation and store it 
+        //const value = operate(operator, a, b)// complete the operation and store it 
         //display(value.toString) // display that total variable
     })
 
